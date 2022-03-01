b60772e57cb43f47a227be1b2eb809945c871919
jacpolanski <jac.polanski@gmail.com>
2022-03-01 18:29:42 +0100

    Asynchronous data problem

--- reeferaggre/pages/api/hello.js
+++ reeferaggre/pages/api/hello.js
@@ -1,5 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const Papa = require("papaparse")
+const fs = require("fs")
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const parseFile = ()  => new Promise((resolve) => {
+    Papa.parse(fs.createReadStream("DailyMonitoring-1911181.csv"), {
+        header: true,
+        download: true,
+        dynamicTyping: true,
+        complete: function(results) {
+            console.log(results);
+            resolve(results)
+
+        }
+    });
+
+})
+
+// console.log(data);
+
+// console.log(data);
+export default async function handler(req, res) {
+    const data = await parseFile()
+    console.log(data);
+    res.status(200).json(data)
 }
